f6f0b3272d9cf4aa9f55059adeb1ba619fa78729
David Bell <dave@evad.info>
2014-02-21 09:35:22 -0500

    Working on manage.py

--- manage.py
+++ manage.py
@@ -7,8 +7,8 @@ app = WrapFlask(__name__)
 manager = Manager(app)
 
 @manager.command
-@manager.option('-s', '--server', help='What server to sync from, default all',required=False,default='')
-def agentsync(server):
+@manager.option('-s', '--server', help='What server to sync from, default all',required=False)
+def agentsync(server=''):
 	## Load servers
 	rows = mysqladm.servers.get_all_servers()
 	
