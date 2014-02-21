fc43510ebf416d351a7717ef5f796c26d01deff0
David Bell <dave@evad.info>
2014-02-21 10:08:57 -0500

    Working on manage.py

--- manage.py
+++ manage.py
@@ -7,13 +7,23 @@ import mysqladm.servers
 import mysqladm.core
 
 app = WrapFlask(__name__)
-manager = Manager(app)
+manager = Manager(app,usage='test',help='test2')
 
 @manager.command
 @manager.option('-s', '--server', help='What server to sync from, default all',required=False)
 def agentsync(server=''):
-	g.db = mysqladm.core.db_connect()	
+	g.db = mysqladm.core.db_connect()
+
+	## Load servers
+	rows = mysqladm.servers.get_all_servers()
+	
+	for row in rows:
+		print row['hostname']
 		
+@manager.command
+def listservers():
+	g.db = mysqladm.core.db_connect()
+
 	## Load servers
 	rows = mysqladm.servers.get_all_servers()
 	
